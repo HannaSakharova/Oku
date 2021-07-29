7c884d5d33c85b15c5026ae14d1f3015e62b3098
Anna <november11131999@gmail.com>
2021-07-29 17:52:42 +0300

    fixed problem with scrolling when the popup is open

--- Oku/js/main.js
+++ Oku/js/main.js
@@ -96,7 +96,6 @@ $(document).ready(function () {
 		};
 	};
 	select();
-
 });
 //FORMS
 function forms() {
@@ -806,34 +805,94 @@ function searchselectreset() {
 		}
 	});
 }
-//Popup registration/authorization
-const class_list = {
-	popup: 'popup',
-	popup_active: 'popup-active',
-	popup_has_scroll: 'popup-has-scroll',
-	popup_body: 'authorization',
-	trigger_open: 'popup-open',
-	trigger_close: 'popup-close'
-};
+//Popup for registration and authorization wondows
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding');
 
-document.addEventListener('click', function (event) {
-	//Open
-	if (event.target.closest(`.${class_list.trigger_open}`)) {
-		event.preventDefault();
+let unlock = true;
 
-		const target = event.target.closest(`.${class_list.trigger_open}`);
-		const popupId = target.getAttribute('href').replace('#', '');
-		const popup = document.getElementById(popupId);
+const timeout = 600;
 
-		popup.classList.add(class_list.popup_active);
+if (popupLinks.length > 0) {
+	for (let index = 0; index < popupLinks.length; index++) {
+		const popupLink = popupLinks[index];
+		popupLink.addEventListener('click', function (e) {
+			const popupName = popupLink.getAttribute('href').replace('#', '');
+			const currentPopup = document.getElementById(popupName);
+			popupOpen(currentPopup);
+			e.preventDefault();
+		});
 	}
+}
 
-	//Close
-	if (event.target.closest(`.${class_list.trigger_close}`) || event.target.clasList.contains(`.${class_list.popup_active}`)) {
-		event.preventDefault();
-		const popup = event.target.closest(`.${class_list.popup_active}`);
-		popup.classList.remove(class_list.popup_active);
+const popupCloseIcon = document.querySelectorAll('.popup-close');
+if (popupCloseIcon.length > 0) {
+	for (let index = 0; index < popupCloseIcon.length; index++) {
+		const el = popupCloseIcon[index];
+		el.addEventListener('click', function (e) {
+			popupClose(el.closest('.popup'));
+			e.preventDefault();
+		});
 	}
-});
+}
 
+function popupOpen(currentPopup) {
+	if (currentPopup && unlock) {
+		const popupActive = document.querySelector('.popup.open');
+		if (popupActive) {
+			popupClose(popupActive, false);
+		} else {
+			bodyLock();
+		}
+		currentPopup.classList.add('open');
+		currentPopup.addEventListener('click', function (e) {
+			if (!e.target.closest('.registration, .authorization')) {
+				popupClose(e.target.closest('.popup'));
+			}
+		});
+	}
+}
+
+function popupClose(popupActive, doUnlock = true) {
+	if (unlock) {
+		popupActive.classList.remove('open');
+		if (doUnlock) {
+			bodyUnLock();
+		}
+	}
+}
+
+function bodyLock() {
+	const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+	if (lockPadding.length > 0) {
+		for (let index = 0; index < lockPadding.length; index++) {
+			const el = lockPadding[index];
+			el.style.paddingRight = lockPaddingValue;
+		}
+	}
+	body.style.paddingRight = lockPaddingValue;
+	body.classList.add('lock');
+
+	unlock = false;
+	setTimeout(function () {
+		unlock = true;
+	}, timeout);
+}
+
+function bodyUnLock() {
+	setTimeout(() => {
+		for (let index = 0; index < lockPadding.length; index++) {
+			const el = lockPadding[index];
+			el.style.paddingRight = '0px'
+		}
+		body.style.paddingRight = '0px';
+		body.classList.remove('lock');
+	}, timeout);
+
+	unlock = false;
+	setTimeout(() => {
+		unlock = true;
+	}, timeout);
+}
 
